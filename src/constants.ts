10bd9182c759f08a2de68110e2b0e8e66a3f022e
andy13shch <andy13shch@gmail.com>
2026-06-29 22:28:09 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const DEFAULT_ARTIST_INFO = {
   name: "AvdeevART",
   bio: "Это краткая биография художника. Вы можете отредактировать ее в панели администратора.",
   email: "artist@example.com",
-  homeHeroBgUrl: "https://images-assets.nasa.gov/image/art002e009298/art002e009298~large.jpg",
+  homeHeroBgUrl: "",
   homeHeroSubtitle: "Исследование границ восприятия через свет, цвет и текстуру.",
   homePortfolioSubtitle: "Коллекция работ, исследующих пересечение света и эмоций.",
 };
